3f04e5a76dfd397a27913b3b899c2168960c4675
Ethereumistic <badjarovv@gmail.com>
2024-04-06 01:59:28 +0300

    added all cons + rough design

--- components/concurents/LokaciiCon.js
+++ components/concurents/LokaciiCon.js
@@ -9,9 +9,12 @@ import { MapContainer, TileLayer, Marker, Popup, useMap, mapConsumer } from 'rea
 import MarkerClusterGroup from 'react-leaflet-cluster';
 import Backlink from '../Backlink';
 import Link from 'next/link';
+import { IoIosArrowDropdownCircle } from "react-icons/io";
+import MarkerGridBlue from './blue/MarkerGridBlue';
+import MarkerGridRed from './red/MarkerGridRed';
 
-const markerb = require('@/components/concurents/pinsBlue');
-const markery = require('@/components/markerz')
+const markerb = require('@/components/concurents/blue/blue');
+const markery = require('@/components/concurents/red/red')
 const markers = require('@/components/markers')
 
 const customIcon = new Icon({
@@ -25,27 +28,48 @@ const customIcon = new Icon({
   })
 
   const customIconConY = new Icon({
-    iconUrl: "https://cdn.jsdelivr.net/gh/Ethereumistic/obshtini/locations/dypin.png",
+    iconUrl: "https://cdn.jsdelivr.net/gh/Ethereumistic/obshtini/locations/lrpin.png",
     iconSize: [34, 34]
   })
 
   function LocationMap({ selectedLocation }) {
-
     const map = useMap();
-  
-    
-  
-  
       if (selectedLocation) {
         const marker = markers.find((marker) => marker.popUp === selectedLocation);
         if (marker) {
           const [lat, lng] = marker.geocode;
           map.setView([lat, lng], 28); // Adjust zoom level as needed
         }
+
       }
     
       return null; // Return null since this component doesn't render anything
     }
+    
+  function LocationMapBlue({ selectedLocationBlue }) {
+    const map = useMap();
+      if (selectedLocationBlue) {
+        const markerb = markers.find((marker) => marker.popUp === selectedLocationBlue);
+        if (markerb) {
+          const [lat, lng] = markerb.geocode;
+          map.setView([lat, lng], 28); // Adjust zoom level as needed
+        }
+      }
+      return null; // Return null since this component doesn't render anything
+    };
+
+    function LocationMapRed({ selectedLocationRed }) {
+        const map = useMap();
+          if (selectedLocationRed) {
+            const markery = markers.find((marker) => marker.popUp === selectedLocationRed);
+            if (markery) {
+              const [lat, lng] = markery.geocode;
+              map.setView([lat, lng], 28); // Adjust zoom level as needed
+            }
+          }
+          return null; // Return null since this component doesn't render anything
+        };
+
 
  
 export default function LokaciiCon({  }) {
@@ -53,11 +77,31 @@ export default function LokaciiCon({  }) {
     const [selectedCity, setSelectedCity] = useState(null);
     const [selectedLocation, setSelectedLocation] = useState(null);
 
+    const [selectedCityBlue, setSelectedCityBlue] = useState(null);
+    const [selectedLocationBlue, setSelectedLocationBlue] = useState(null);
+
+    const [selectedCityRed, setSelectedCityRed] = useState(null);
+    const [selectedLocationRed, setSelectedLocationRed] = useState(null);
+
     const handleCityClick = (cityName) => {
       if (selectedCity === cityName) {
         setSelectedCity(null); // Close the menu if the same city is clicked again
       } else {
         setSelectedCity(cityName);
+      }
+    };
+    const handleCityClickBlue = (cityName) => {
+      if (selectedCityBlue === cityName) {
+        setSelectedCityBlue(null); // Close the menu if the same city is clicked again
+      } else {
+        setSelectedCityBlue(cityName);
+      }
+    };
+    const handleCityClickRed = (cityName) => {
+      if (selectedCityRed === cityName) {
+        setSelectedCityRed(null); // Close the menu if the same city is clicked again
+      } else {
+        setSelectedCityRed(cityName);
       }
     };
   
@@ -69,33 +113,72 @@ export default function LokaciiCon({  }) {
       if (marker) {
         const [lat, lng] = marker.geocode;
 
-
         // map.setView([lat, lng], 14); // Adjust zoom level as needed
       }
     };
 
+    const handleLocationClickRed = (location) => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+        setSelectedLocation(location);
+      const markery = markers.find((marker) => marker.popUp === location);
+      if (markery) {
+        const [lat, lng] = markery.geocode;
+          // map.setView([lat, lng], 14); 
+        }
+      };
+      
+      const handleLocationClickBlue = (location) => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+        setSelectedLocation(location);
+        const markerb = markers.find((marker) => marker.popUp === location);
+        if (markerb) {
+          const [lat, lng] = markerb.geocode;
+        //   map.setView([lat, lng], 14); 
+        }
+      };
+
     const uniqueCityNames = [...new Set(markers.map((marker) => marker.city))];
+    const uniqueCityNamesBlue = [...new Set(markerb.map((marker) => marker.city))];
+    const uniqueCityNamesRed = [...new Set(markery.map((marker) => marker.city))];
 
     const [showMarkers1, setShowMarkers1] = useState(true);
     const [showMarkers2, setShowMarkers2] = useState(true);
     const [showMarkers3, setShowMarkers3] = useState(true);
     const [showMarkers4, setShowMarkers4] = useState(true);
+    const [showRedSidebar, setShowRedSidebar] = useState(true);
+    const [showGreenSidebar, setShowGreenSidebar] = useState(true);
+    const [showBlueSidebar, setShowBlueSidebar] = useState(true);
+    const [color, setColor] = useState(true);
 
+    
     const toggleMarkers1 = () => {
       setShowMarkers1(prevState => !prevState);
     };
-  
     const toggleMarkers2 = () => {
       setShowMarkers2(prevState => !prevState);
     };
-  
     const toggleMarkers3 = () => {
       setShowMarkers3(prevState => !prevState);
     };
-
     const toggleMarkers4 = () => {
         setShowMarkers4(prevState => !prevState);
       };
+
+
+      const toggleRedSidebar = () => {
+        setShowRedSidebar((prev) => !prev);
+      };
+      const toggleGreenSidebar = () => {
+        setShowGreenSidebar((prev) => !prev);
+      };
+      const toggleBlueSidebar = () => {
+        setShowBlueSidebar((prev) => !prev);
+      };
+
+      const toggleColor = () => {
+        setColor((prev) => !prev);
+      };
+    
 
 
     return (
@@ -104,72 +187,6 @@ export default function LokaciiCon({  }) {
                     
         
         <div className='w-10/12 translate-x-10 sm:translate-x-12 md:translate-x-16 lg:translate-x-72 xl:translate-x-72 2xl:translate-x-72 '>
-        {/* <MapContainer
-        
-        center={[42.7339, 25.4858]}
-        zoom={8}
-        style={{ width: '100%', height: '800px', zIndex: '10' }}
-        fullscreenControl={ true } >
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        <MarkerClusterGroup
-        chunkedLoading>
-        {markers.map((marker, index) => (
-        <Marker key={index} position={marker.geocode} icon={customIconConB}>
-        <Popup>
-            <div className=''>
-                <p className='font-russo text-3xl text-center text-lblue bg-dblue rounded-full py-2'>{marker.city}</p>
-                <p className='font-osw text-lg text-center text-dgreen'>{marker.popUp}</p>
-                <p className='font-osw text-lg text-center text-dgreen'>
-                    {marker.geocode[0].toFixed(6)} {marker.geocode[1].toFixed(6)}
-                </p>
-            </div>
-        </Popup>
-
-
-        </Marker>
-))}
-        </MarkerClusterGroup>
-        <LocationMap selectedLocation={selectedLocation} />
-
-
-        
-        </MapContainer> */}
-
-{/* <MapContainer center={[42.7339, 25.4858]} zoom={8} style={{ width: '100%', height: '800px' }}>
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        <MarkerClusterGroup>
-          {showMarkers1 && markerb.map((marker, index) => (
-            <Marker key={index} position={marker.geocode} icon={customIconConB}>
-              <Popup>
-                <div>
-                  <p>{marker.city}</p>
-                  <p>{marker.popUp}</p>
-                </div>
-              </Popup>
-            </Marker>
-          ))}
-          {showMarkers2 && markery.map((marker, index) => (
-            <Marker key={index} position={marker.geocode} icon={customIconConY}>
-              <Popup>
-                <div>
-                  <p>{marker.city}</p>
-                  <p>{marker.popUp}</p>
-                </div>
-              </Popup>
-            </Marker>
-          ))}
-          {showMarkers3 && markers.map((marker, index) => (
-            <Marker key={index} position={marker.geocode} icon={customIcon}>
-              <Popup>
-                <div>
-                  <p>{marker.city}</p>
-                  <p>{marker.popUp}</p>
-                </div>
-              </Popup>
-            </Marker>
-          ))}
-        </MarkerClusterGroup>
-      </MapContainer> */}
 
     {/* THE MAP CONDITIONAL RENDER BASED ON MARKERCLUSTERTOGGLE BUTTON */}
     {showMarkers4 ? (
@@ -207,6 +224,9 @@ export default function LokaciiCon({  }) {
             </Marker>
           ))}
         </MarkerClusterGroup>
+        <LocationMap selectedLocation={selectedLocation} />
+        <LocationMapBlue selectedLocationBlue={selectedLocationBlue} />
+        <LocationMapRed selectedLocationRed={selectedLocationRed} />
       </MapContainer>
       ) : (
         <MapContainer center={[42.7339, 25.4858]} zoom={8} style={{ width: '100%', height: '800px' }}>
@@ -245,27 +265,49 @@ export default function LokaciiCon({  }) {
             </div>
 
             <div className='sm:translate-y-2 md:translate-y-2 lg:-translate-y-[800px] xl:-translate-y-[800px] 2xl:-translate-y-[800px] sm:relative md:relative lg:fixed xl:fixed 2xl:fixed '>
-            <div className='flex space-x-1 justify-center my-8'>
-<button onClick={toggleMarkers1} className={`font-osw text-xl text-center text-dblue bg-lblue hover:scale-110 transition duration-300 p-2 rounded-lg ${showMarkers1 ? 'bg-lblue' : 'bg-gray-400'}`}>
-  <h4>🌊</h4>
-</button>
+            <div className='flex space-x-1 justify-center my-4'>
+                <div className='flex '>
+                <div className='flex-col flex'>
+  <button onClick={toggleMarkers1} className={`font-osw text-xl text-center bg-lblue hover:scale-110 transition duration-300 p-2 rounded-lg ${showMarkers1 ? 'text-dblue bg-lblue' : 'text-gray-400 bg-gray-300'}`}>
+    <h4>🌊</h4>
+  </button>
+  <button onClick={toggleBlueSidebar} className={`font-osw text-xl text-center bg-lblue hover:scale-110 transition duration-300 p-2 rounded-lg ${showBlueSidebar ? 'text-dblue bg-lblue' : 'text-gray-400 bg-gray-300'}`}>
+    <h4><IoIosArrowDropdownCircle size={24}/></h4>
+  </button>
+</div>
 
-      <button onClick={toggleMarkers2} className={`font-osw text-xl text-center text-dyellow bg-lyellow  hover:scale-110 transition duration-300 p-2 rounded-lg ${showMarkers2 ? 'bg-lyellow' : 'bg-gray-400'}`}>
-        <h4 >❌</h4>
-      </button>
-      <button onClick={toggleMarkers3} className={`font-osw text-xl text-center text-dgreen bg-lgreen  hover:scale-110 transition duration-300 p-2 rounded-lg ${showMarkers3 ? 'bg-lblue' : 'bg-gray-400'}`}>
-        <h4 className=''>🟢</h4>
-      </button>
+<div className='flex-col flex'>
+  <button onClick={toggleMarkers2} className={`font-osw text-xl text-center bg-lyellow hover:scale-110 transition duration-300 p-2 rounded-lg ${showMarkers2 ? 'text-dyellow bg-lyellow' : 'text-gray-400 bg-gray-300'}`}>
+    <h4>❌</h4>
+  </button>
+  <button onClick={toggleRedSidebar} className={`font-osw text-xl text-center bg-lyellow hover:scale-110 transition duration-300 p-2 rounded-lg ${showRedSidebar ? 'text-dyellow bg-lyellow' : 'text-gray-400 bg-gray-300'}`}>
+    <h4><IoIosArrowDropdownCircle size={24}/></h4>
+  </button>
+</div>
 
-      <button onClick={toggleMarkers4} className={`font-osw text-xl text-center text-dgreen bg-lred  hover:scale-110 transition duration-300 p-2 rounded-lg ${showMarkers4 ? 'bg-lred' : 'bg-gray-400'}`}>
-        <h4 className=''>💢</h4>
-      </button>
+<div className='flex-col flex'>
+  <button onClick={toggleMarkers3} className={`font-osw text-xl text-center bg-lgreen hover:scale-110 transition duration-300 p-2 rounded-lg ${showMarkers3 ? 'text-dgreen bg-lgreen' : 'text-gray-400 bg-gray-300'}`}>
+    <h4>🟢</h4>
+  </button>
+  <button onClick={toggleGreenSidebar} className={`font-osw text-xl text-center bg-lgreen hover:scale-110 transition duration-300 p-2 rounded-lg ${showGreenSidebar ? 'text-dgreen bg-lgreen' : 'text-gray-400 bg-gray-300'}`}>
+    <h4><IoIosArrowDropdownCircle size={24}/></h4>
+  </button>
+</div>
+
+<div className='flex-col flex'>
+  <button onClick={toggleMarkers4} className={`font-osw text-xl text-center bg-lred hover:scale-110 transition duration-300 p-2 rounded-lg ${showMarkers4 ? 'text-dgreen bg-lred' : 'text-gray-400 bg-gray-300'}`}>
+    <h4>💢</h4>
+  </button>
+</div>
+
+
+      </div>
         </div>
-            <div className="bg-lblue  dark:lblue px-2 rounded-md w-80 sm:w-96 md:w-96 lg:w-72 xl:w-72 2xl:w-72 translate-x-10 sm:translate-x-32 md:translate-x-64 lg:translate-x-0 xl:translate-x-0 2xl:translate-x-0 h-[800px] overflow-auto custom-scrollbar " >
+            <div className="  dark:lblue px-2 rounded-md w-80 sm:w-96 md:w-96 lg:w-72 xl:w-72 2xl:w-72 translate-x-10 sm:translate-x-32 md:translate-x-64 lg:translate-x-0 xl:translate-x-0 2xl:translate-x-0 h-[800px] overflow-auto custom-scrollbar " >
 
-<h2 className="text-xl pl-20 text-dgreen font-russo mt-4 mb-6">Локации</h2>
+<h2 className="text-xl pl-20 text-dgreen dark:text-white font-russo mt-4 mb-6">Локации</h2>
 <ul className=''>
-  {uniqueCityNames.map((cityName, index) => (
+  {/* {uniqueCityNames.map((cityName, index) => (
     <li key={index} className=" pl-16 py-2 border-dgreen border  w-full">
       <div
         className="flex items-center font-russo cursor-pointer hover:scale-105 active:scale-105 transition duration-300 text-dgreen dark:text-dgreen"
@@ -297,13 +339,133 @@ export default function LokaciiCon({  }) {
         </ol>
       )}
     </li>
-  ))}
+  ))} */}
+
+{showBlueSidebar ? (
+    <div className='bg-lblue rounded-md'>
+    {/* Green Sidebar Content */}
+    {uniqueCityNamesBlue.map((cityName, index) => (
+    <li key={index} className=" pl-16 py-2 border-dgreen border  w-full">
+    <div
+      className="flex items-center font-russo cursor-pointer hover:scale-105 active:scale-105 transition duration-300 text-dred dark:text-dred"
+      onClick={() => handleCityClick(cityName)}
+      onTouchStart={() => handleCityClick(cityName)}
+       >
+
+      {cityName}
+      {selectedCity === cityName ?  (
+        <FaAngleDown className="w-4 h-4 ml-2" />
+      ) : (
+        <FaAngleRight className="w-4 h-4 ml-2" />
+      )}
+    </div>
+    {selectedCity === cityName && (
+      <ol className="ml-4  cursor-pointer ">
+        {markerb
+          .filter((marker) => marker.city === cityName)
+          .map((marker, index) => (
+              <li
+              key={index}
+              className="mb-1 list-decimal cursor-pointer text-dgreen dark:text-dgreen font-osw "
+              onClick={() => handleLocationClickBlue(marker.popUp)}
+              onTouchStart={() => handleLocationClickBlue(marker.popUp)}
+              >
+              {marker.popUp}
+            </li>
+          ))}
+      </ol>
+    )}
+  </li>
+))}
+  </div>
+) : null}
+
+{showRedSidebar ? (
+  <div className='bg-lred rounded-md'>
+    {/* Red Sidebar Content */}
+    {uniqueCityNamesRed.map((cityName, index) => (
+      <li key={index} className="pl-16 py-2 border-dgreen border w-full">
+        <div
+          className="flex items-center font-russo cursor-pointer hover:scale-105 active:scale-105 transition duration-300 text-dred dark:text-dred"
+          onClick={() => handleCityClick(cityName)}
+          onTouchStart={() => handleCityClick(cityName)}
+        >
+          {cityName}
+          {selectedCity === cityName ?  (
+            <FaAngleDown className="w-4 h-4 ml-2" />
+          ) : (
+            <FaAngleRight className="w-4 h-4 ml-2" />
+          )}
+        </div>
+        {selectedCity === cityName && (
+          <ol className="ml-4 cursor-pointer">
+            {markery
+              .filter((marker) => marker.city === cityName)
+              .map((marker, index) => (
+                <li
+                  key={index}
+                  className="mb-1 list-decimal cursor-pointer text-dgreen dark:text-dgreen font-osw"
+                  onClick={() => handleLocationClickRed(marker.popUp, marker)}
+                  onTouchStart={() => handleLocationClickRed(marker.popUp, marker)}
+                >
+                  {marker.popUp}
+                </li>
+              ))}
+          </ol>
+        )}
+      </li>
+    ))}
+  </div>
+) : null}
+
+{showGreenSidebar ? (
+    <div className='bg-lgreen rounded-md'>
+    {/* Green Sidebar Content */}
+    {uniqueCityNames.map((cityName, index) => (
+    <li key={index} className=" pl-16 py-2 border-dgreen border  w-full">
+    <div
+      className="flex items-center font-russo cursor-pointer hover:scale-105 active:scale-105 transition duration-300 text-dred dark:text-dred"
+      onClick={() => handleCityClick(cityName)}
+      onTouchStart={() => handleCityClick(cityName)}
+       >
+
+      {cityName}
+      {selectedCity === cityName ?  (
+        <FaAngleDown className="w-4 h-4 ml-2" />
+      ) : (
+        <FaAngleRight className="w-4 h-4 ml-2" />
+      )}
+    </div>
+    {selectedCity === cityName && (
+      <ol className="ml-4  cursor-pointer ">
+        {markers
+          .filter((marker) => marker.city === cityName)
+          .map((marker, index) => (
+              <li
+              key={index}
+              className="mb-1 list-decimal cursor-pointer text-dgreen dark:text-dgreen font-osw "
+              onClick={() => handleLocationClick(marker.popUp)}
+              onTouchStart={() => handleLocationClick(marker.popUp)}
+              >
+              {marker.popUp}
+            </li>
+          ))}
+      </ol>
+    )}
+  </li>
+))}
+  </div>
+) : null}
+
 </ul>
+
 </div>
     
             </div>
             <div className='w-3/5 translate-x-80 translate-y-[820px]'>        
-    <MarkerGrid markers={markers} />
+            {showGreenSidebar && <MarkerGrid markers={markers} />}
+{showBlueSidebar && <MarkerGridBlue markers={markerb} />}
+{showRedSidebar && <MarkerGridRed markers={markery} />}
     </div>
     <Backlink page="locations" />
         </div>
---
+++ components/concurents/blue/MarkerGridBlue.js
@@ -0,0 +1,205 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { BiCurrentLocation } from 'react-icons/bi';
+import { TbCurrentLocation } from 'react-icons/tb';
+import { FaLocationArrow } from 'react-icons/fa';
+import CitySearchBox from '@/components/CitySearchBox';
+
+
+
+const MarkerGridBlue = ({ markers }) => {
+  const [copiedText, setCopiedText] = useState(null);
+  const [showPopup, setShowPopup] = useState(false);
+
+  const cityRefs = {}; 
+
+  const handleCitySearch = (city) => {
+    const cityRef = cityRefs[city];
+    if (cityRef) {
+      const offset = -120; // Adjust the offset value as needed
+      const topPos = cityRef.getBoundingClientRect().top + window.pageYOffset + offset;
+      window.scrollTo({ top: topPos, behavior: 'smooth' });
+    }
+  };
+  
+
+
+  // const handleCityClick = (cityName) => {
+  //   const cityRef = cityRefs[cityName];
+  //   if (cityRef) {
+  //     cityRef.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  //   }
+  // };
+
+  useEffect(() => {
+    if (copiedText) {
+      setShowPopup(true);
+      setTimeout(() => {
+        setShowPopup(false);
+      }, 1800); // Adjust the duration as needed (in milliseconds)
+    }
+  }, [copiedText]);
+
+
+  // Group markers by city
+  const groupedMarkers = markers.reduce((acc, test) => {
+    const { city } = test;
+    if (!acc[city]) {
+      acc[city] = [];
+    }
+    acc[city].push(test);
+    return acc;
+  }, {});
+  
+
+
+
+  const copyToClipboard = (text) => {
+    navigator.clipboard.writeText(text);
+    setCopiedText(text);
+    setTimeout(() => {
+      setCopiedText(null);
+    }, 500);
+  };
+
+  return (
+    <div className='mb-8'>
+      
+      <div className='text-center'>
+        
+      </div>
+    <div className="border-2 rounded-lg border-dgreen dark:border-lblue p-4 -mx-16 sm:-mx-16 md:-mx-16 lg:-mx-0 xl:-mx-0 z-20 -translate-y-[800px] -translate-x-60 sm:-translate-x-48 md:-translate-x-36 lg:-translate-x-0 xl:-translate-x-0">
+    <CitySearchBox cities={Object.keys(groupedMarkers)} onCitySearch={handleCitySearch} />
+    {Object.keys(groupedMarkers).map((city, index) => (
+  <div key={city} className="border rounded-lg border-dgreen dark:border-lblue p-8 mb-4 " ref={(el) => (cityRefs[city] = el)}>
+          <h2 className="text-4xl text-center mt-2 font-russo mb-2 dark:text-gray-200 ">{city}</h2>
+          <h1 className="text-center text-2xl mt-4 font-russo text-dgreen dark:text-white mb-2">
+            {groupedMarkers[city].length}
+          </h1>
+          <h2 className="text-xl font-russo -mb-8 dark:text-white">Адрес:</h2>
+          <h2 className="text-xl text-right font-russo mb-6 dark:text-white 2xl:mr-8 mr-0">GPS:</h2>
+          {groupedMarkers[city].map((location, locationIndex) => (
+            <div key={locationIndex} className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3'>
+
+            {/* COPY ADDRESS BUTTON */}
+            <form
+              className='mt-2 sm:w-[60px] sm:h-[60px] w-[32px] h-[32px] sm:pl-[14px] pl-[4px] 
+                        border-2 rounded-md
+                        border-lblue
+                        translate-y-16
+                          sm:translate-y-0
+                          md:translate-y-0
+                          lg:translate-y-0
+                          xl:translate-y-0
+                          2xl:translate-y-0
+                          -ml-4
+                          sm:ml-0
+                          text-lblue
+                          cursor-pointer
+                          hover:scale-110
+                          transition
+                          duration-300'
+              onClick={() => {
+                copyToClipboard(location.popUp);
+              }}
+              onTouchStart={() => {
+                copyToClipboard(location.popUp);
+              }}
+            >
+              <FaLocationArrow size={20} className='dark:block hidden mt-1 sm:mt-4' />
+              <FaLocationArrow size={20} className='dark:hidden mt-1 sm:mt-4' />
+            </form>
+          
+            {/* Middle button for copying address and geocode */}
+            <form
+              key={locationIndex}
+              className={`mx-8
+                          sm:-mx-8 
+                          md:-mx-16 
+                          lg:-mx-24 
+                          xl:-mx-36 
+                          2xl:-mx-36 
+                          sm:-translate-x-24
+                          md:-translate-x-0
+                          lg:-translate-x-2
+                          xl:-translate-x-2
+                          2xl:-translate-x-4
+                          justify-center p-2 rounded-lg 
+                          bg-lblue dark:lgreen mb-2 
+                          text-center transition 
+                          cursor-pointer
+                          hover:scale-110
+                          duration-300
+                          ${copiedText === `${location.popUp}\n ${location.geocode[0]}, ${location.geocode[1]}` ? 'animate-ping' : ''}`}
+              onClick={() => {
+                copyToClipboard(`${location.popUp}\n ${location.geocode[0]}, ${location.geocode[1]}`);
+              }}
+              onTouchStart={() => {
+                copyToClipboard(`${location.popUp}\n ${location.geocode[0]}, ${location.geocode[1]}`);
+              }}
+            >
+              <p className={`mt-2 mb-4 font-russo text-left  ${copiedText === `${location.popUp}\n ${location.geocode[0]}, ${location.geocode[1]}` ? 'hidden' : 'block'} dark:text-dgreen `}>
+                {location.popUp}
+              </p>
+              <p className={`text-right font-osw sm:-mt-4 sm:mb-2 hover:text-green ${copiedText === `${location.popUp}\n ${location.geocode[0]}, ${location.geocode[1]}` ? 'hidden' : 'block'} dark:text-dgreen`}>
+                {location.geocode[0]}, {location.geocode[1]}
+              </p>
+              {copiedText === `${location.popUp}\n ${location.geocode[0]}, ${location.geocode[1]}` && (
+                <p className="text-3xl font-russo text-dgreen my-[14px] text-center dark:text-dgreen">Копирано!</p>
+              )}
+            </form>
+          
+            {/* Right button for copying geocode */}
+            <button
+              className=' sm:w-[60px] sm:h-[60px] 
+                          w-[32px] h-[32px] 
+                          sm:pl-[12px] 
+                          border-2 rounded-md 
+                          border-lblue
+                          translate-x-60 
+                          sm:translate-x-[410px] 
+                          -translate-y-24
+                          sm:-translate-y-24
+                          md:-translate-y-0
+                          lg:-translate-y-0
+                          xl:-translate-y-0
+                          2xl:-translate-y-0
+                          md:translate-x-36 
+                          lg:translate-x-40 
+                          xl:translate-x-48 
+                          2xl:translate-x-64
+                          mt-2
+                          ml-3
+                          text-lblue
+                          cursor-pointer
+                          hover:scale-110
+                          transition
+                          duration-300
+                          '
+              onClick={() => {
+                copyToClipboard(`${location.geocode[0]}, ${location.geocode[1]}`);
+              }}
+              onTouchStart={() => {
+                copyToClipboard(`${location.geocode[0]}, ${location.geocode[1]}`);
+              }}
+            >
+              <TbCurrentLocation size={28} className='dark:block hidden text-lblue' />
+              <BiCurrentLocation size={28} className='dark:hidden block text-lblue' />
+            </button>
+          </div>
+          ))}
+        </div>
+      ))}
+
+{/* {showPopup && (
+  <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 bg-gray-200 dark:bg-dgreen py-1 px-4 rounded-lg shadow-lg animate-fade-out">
+    <p className="text-sm dark:text-gray-300">Копирано!</p>
+  </div>
+)} */}
+
+
+    </div>
+    </div>
+  );
+};
+
+export default MarkerGridBlue;
